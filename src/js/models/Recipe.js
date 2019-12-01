a097a680e99dfa1651a9c8960dcdcd2a7764790f
valonkadriaj <valon_kadriaj@outlook.com>
2019-12-01 02:54:16 +0100

    clean code

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -20,7 +20,6 @@ export default class Recipe {
     }
 
     calcTime() {
-        // Assuming that we need 15 min for each 3 ingredients
         const numIng = this.ingredients.length;
         const periods = Math.ceil(numIng / 3);
         this.time = periods * 15;
